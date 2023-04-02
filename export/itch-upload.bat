258a2b118b2581451a3c50102c0ff1d65e43c315
YellowAfterlife <yellowafterlife@hotmail.com>
2023-04-03 00:29:29 +0300

    Updated the itch upload script

--- export/itch-upload.bat
+++ export/itch-upload.bat
@@ -2,9 +2,9 @@
 set /p ver="Version?: "
 echo Uploading %ver%...
 set user=yellowafterlife
-set ext=gamemaker-native_cursor
-cmd /C itchio-butler push native_cursor-for-GMS1.gmez %user%/%ext%:gms1 --userversion=%ver%
-cmd /C itchio-butler push native_cursor-for-GMS2.yymp %user%/%ext%:gms2 --userversion=%ver%
-cmd /C itchio-butler push native_cursor-for-GMS2.3+.yymps %user%/%ext%:gms2.3 --userversion=%ver%
+set ext=gamemaker-native-cursors
+::cmd /C itchio-butler push native_cursor-for-GMS1.gmez %user%/%ext%:gms1 --userversion=%ver%
+::cmd /C itchio-butler push native_cursor-for-GMS2.yymp %user%/%ext%:gms2 --userversion=%ver%
+cmd /C itchio-butler push native_cursor-for-GM2022+.yymps %user%/%ext%:gm2022 --userversion=%ver%
 
 pause
